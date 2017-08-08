7d85c6e59c97b426a936ae4c0eadd29617ca1fd8
Spencer Oakes <spencer.oakes84@gmail.com>
2017-08-08 16:27:16 -0400

    * .gitignore: * .bowerrc: * Program.cs: * Startup.cs: * bower.json: * appsettings.json: * StoreUser.cs: * bundleconfig.json: * site.js: * favicon.ico: * ALittleExtra.csproj: * site.css: * site.min.js: * LoginRequest.cs: * _ViewStart.cshtml: * ApplicationUser.cs: * Index.cshtml: * About.cshtml: * site.min.css: * Error.cshtml: * RegisterRequest.cs: * Contact.cshtml: * _ViewImports.cshtml: * banner3.svg: * banner2.svg: * banner1.svg: * banner4.svg: * _Layout.cshtml: * appsettings.Development.json: * LICENSE: * HomeController.cs: * .bower.json: * LICENSE.txt: * FoodController.cs: * .bower.json: * jquery.js: * npm.js: * jquery.min.js: * AccountsController.cs: * jquery.min.map: * LICENSE.md: * .bower.json: * bootstrap.js: * bootstrap.css: * bootstrap.min.js: * bootstrap.css.map: * bootstrap.min.css: * bootstrap-theme.css: * bootstrap.min.css.map: * jquery.validate.js: * .bower.json: * bootstrap-theme.min.css: * bootstrap-theme.css.map: * additional-methods.js: * jquery.validate.min.js: * bootstrap-theme.min.css.map: * additional-methods.min.js: * glyphicons-halflings-regular.svg: * glyphicons-halflings-regular.ttf: * glyphicons-halflings-regular.eot: * glyphicons-halflings-regular.woff: * glyphicons-halflings-regular.woff2: * jquery.validate.unobtrusive.js: * jquery.validate.unobtrusive.min.js:
    * ALittleExtra.sln: Initial Commit/Set up

---
+++ ALittleExtra/Models/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ALittleExtra.Models
+{
+    public class LoginRequest
+    {
+		public string Email { get; set; }
+		public string Password { get; set; }
+    }
+}
---
+++ ALittleExtra/Models/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ALittleExtra.Models
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
