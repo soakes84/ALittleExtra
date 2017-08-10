733fa209f0a5893c6b25413302654555d908e9c1
Spencer Oakes <spencer.oakes84@gmail.com>
2017-08-10 16:05:56 -0400

    started setting up database

--- ALittleExtra/Data/ALittleMoreContext.cs
+++ ALittleExtra/Data/ALittleMoreContext.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+               
 namespace ALittleExtra.Data
 {
-    public class ALittleMoreContext
+    public class ALittleMoreContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<TotalFood> TotalFood { get; set; }
+        public DbSet<BoxFood> BoxFood { get; set; }
+        public DbSet<CanFood> CanFood { get; set; }
+        public DbSet<Fruit> Fruit { get; set; }
+        public DbSet<Meat> Meat { get; set; }
+        public DbSet<Vegetables> Vegetables { get; set; }
+        public DbSet<HighPriority> HighPriority { get; set; }
+        public DbSet<LowPriority> LowPriority { get; set; }
+
         public ALittleMoreContext()
         {
         }
