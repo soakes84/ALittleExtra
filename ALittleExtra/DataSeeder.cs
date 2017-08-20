6ff6d8565871805ce0fdb0890e07cff67783c993
Spencer Oakes <spencer.oakes84@gmail.com>
2017-08-20 16:46:56 -0400

    Started to add seed data/dummy data

---
+++ ALittleExtra/DataSeeder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ALittleExtra.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ALittleExtra
+{
+    public static class DataSeeder
+    {
+        public static async void SeedData(this IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.GetRequiredService<ALittleExtraContext>();
+            var userManager = app.ApplicationServices.GetRequiredService<UserManager<ApplicationUser>>();
+            var storeManager = app.ApplicationServices.GetRequiredService<UserManager<StoreUser>>();
+
+            var user = await userManager.FindByEmailAsync("food@food.com");
+            if (user == null)
+            {
+                
+            }
+        }
+    }
+}
