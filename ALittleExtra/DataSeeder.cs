80b2872d3dd455258b130acdfadef5fe4da314f2
Spencer Oakes <spencer.oakes84@gmail.com>
2017-08-22 19:22:02 -0400

    More Dummy Data, added store user and more food

--- ALittleExtra/DataSeeder.cs
+++ ALittleExtra/DataSeeder.cs
@@ -21,6 +21,7 @@ namespace ALittleExtra
 				user.Email = "food@food.com";
 				user.UserName = "Foodie123";
 				await userManager.CreateAsync(user, "Testtest1");
+
 				var food = new TotalFood() { Type = "Meat" };
                 food.UserName = user.UserName;
                 user.TotalFood.Add(food);
@@ -28,6 +29,67 @@ namespace ALittleExtra
 				meat.Owner = user;
                 meat.UserName = user.UserName;
                 context.Meat.Add(meat);
+
+				var food1 = new TotalFood() { Type = "Meat" };
+				food1.UserName = user.UserName;
+				user.TotalFood.Add(food1);
+				var meat1 = new Meat();
+				meat1.Owner = user;
+				meat1.UserName = user.UserName;
+				context.Meat.Add(meat1);
+
+				var food2 = new TotalFood() { Type = "Meat" };
+				food2.UserName = user.UserName;
+				user.TotalFood.Add(food2);
+				var meat2 = new Meat();
+				meat2.Owner = user;
+				meat2.UserName = user.UserName;
+				context.Meat.Add(meat2);
+
+				var food3 = new TotalFood() { Type = "Fruit" };
+				food3.UserName = user.UserName;
+				user.TotalFood.Add(food3);
+				var fruit = new Fruit();
+                fruit.Owner = user;
+                fruit.UserName = user.UserName;
+				context.Fruit.Add(fruit);
+
+				var food4 = new TotalFood() { Type = "Fruit" };
+				food4.UserName = user.UserName;
+				user.TotalFood.Add(food4);
+				var fruit1 = new Fruit();
+				fruit1.Owner = user;
+				fruit1.UserName = user.UserName;
+				context.Fruit.Add(fruit1);
+
+				var food5 = new TotalFood() { Type = "CanFood" };
+				food5.UserName = user.UserName;
+				user.TotalFood.Add(food5);
+                var canFood = new CanFood();
+                canFood.Owner = user;
+                canFood.UserName = user.UserName;
+                context.CanFood.Add(canFood);
+
+				var food6 = new TotalFood() { Type = "Dairy" };
+                food6.UserName = user.UserName;
+				user.TotalFood.Add(food6);
+                var dairy = new Dairy();
+				dairy.Owner = user;
+				dairy.UserName = user.UserName;
+                context.Dairy.Add(dairy);
+
+				var food7 = new TotalFood() { Type = "Dairy" };
+				food7.UserName = user.UserName;
+				user.TotalFood.Add(food7);
+				var dairy1 = new Dairy();
+				dairy1.Owner = user;
+				dairy1.UserName = user.UserName;
+                context.Dairy.Add(dairy1);
+
+				var storeUser = new StoreUser();
+                storeUser.Email = "store@store.com";
+                storeUser.UserName = "Ukrops";
+                await storeManager.CreateAsync(storeUser, "Testtest1");
             }
         }
     }
