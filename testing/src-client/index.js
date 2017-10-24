edbf1ff5e91bea563b3a71bdca912b80d842b1d4
Aaron LeCheminant <aaronjlech@gmail.com>
2017-10-24 14:22:14 -0400

    adding updated styles

--- testing/src-client/index.js
+++ testing/src-client/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import Login from './containers/Login';
-
+import './styles/styles.scss';
 
 
 
