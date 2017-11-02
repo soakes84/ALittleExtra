c01a8b869b594b0ac1812183af6570f5d2c1c2cf
Aaron LeCheminant <aaronjlech@gmail.com>
2017-11-02 14:01:01 -0400

    adding styles for google input

--- testing/src-client/components/GoogleInput.js
+++ testing/src-client/components/GoogleInput.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+import React from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 
 class GoogleInput extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = { address: '' }
-    this.onChange = (address) => {
-      geocodeByAddress(this.state.address)
-        .then(results => getLatLng(results[0]))
-        .then(latLng => console.log('Success', latLng))
-        .catch(error => console.error('Error', error))
-      this.setState({ address })
-    }
+    super(props);
+    this.state = { address: "" };
+    this.onChange = address => {
+      geocodeByAddress(this.state.address);
+      this.setState({ address });
+    };
   }
 
-  handleFormSubmit = (event) => {
-    event.preventDefault()
+  handleFormSubmit = event => {
+    event.preventDefault();
 
     geocodeByAddress(this.state.address)
       .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error))
-  }
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
 
   render() {
     const inputProps = {
       value: this.state.address,
-      onChange: this.onChange,
-    }
-
-    return (
-        <PlacesAutocomplete inputProps={inputProps} />
-    )
+      onChange: this.onChange
+    };
+    const cssClasses = {
+      root: "form-group",
+      input: "form-control",
+      autocompleteContainer: "autocomplete-container"
+    };
+    return <PlacesAutocomplete inputProps={inputProps} classNames={cssClasses} />;
   }
 }
 
-export default GoogleInput
+export default GoogleInput;
